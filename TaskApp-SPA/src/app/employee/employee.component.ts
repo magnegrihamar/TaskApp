44d8078edaff96234105e5eeaa7df71eacc7335f
Magne Grihamar <magnegrihamar@Magnes-MBP.lan>
2020-02-07 21:13:15 +0100

    Initial Commit

---
+++ TaskApp-SPA/src/app/employee/employee.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+
+@Component({
+  selector: 'app-employee',
+  templateUrl: './employee.component.html',
+  styleUrls: ['./employee.component.css']
+})
+export class EmployeeComponent implements OnInit {
+  employees: any;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.getValues();
+  }
+
+  getValues()
+  {
+    this.http.get('http://localhost:5000/api/employees').subscribe(response => 
+    { 
+      this.employees = response;
+    }, error => 
+    {
+      console.log(error);
+    }) 
+
+  }
+
+}
+
